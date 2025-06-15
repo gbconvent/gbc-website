526095617ebd20923e5377e1444c449a633ad8b0
GB Convent <gbconventetah@gmail.com>
2025-06-15 14:13:40 +0530

    more cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Academics from './pages/Academics';
